928eac5295533a11e82b47e6c02ec10370e87694
philipppahl <ph.pahl@gmail.com>
2019-08-14 11:59:31 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='psycopg2',
+    version='2.8.3',
+    description='psycopg2',
+    author='author_psycopg2',
+    maintainer='maintainer_psycopg2',
+    maintainer_email='email_maintainer',
+    url='https://github.com/philipppahl/awslambda-psycopg2',
+    license='Apache',
+    packages=["psycopg2"],
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+    # install_requires=['requests'],
+)
